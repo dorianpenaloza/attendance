30bcf53ad964413093ab5330d580c6969f8576dd
dorianpenaloza <dorianpenaloza@yahoo.com>
2016-08-11 20:23:15 -0500

    Initial Commit attendace app

---
+++ index.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+//routes
+const index = require('./routes/index')
+const names = require('./routes/names');
+
+// Initialize app
+const app = express();
+
+//Add middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set views directory
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use('/', index);
+app.use('/names', names);
+
+// Set up our server
+const server = http.createServer(app);
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server listening on: ${port}`);
+});
---
+++ routes/names.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+
+
+const names = { }; //create an empty object
+
+router.get('/', function(req, res, next) {
+  res.render('index.ejs', { names }); //renders the 'names' object into index.ejs
+});
+
+router.post('/', function(req, res, next) {
+  const name = req.body.name; // passes the posted 'name' from the form set up in the index.ejs
+
+    //logic to receive and increase names
+    if (names[name]) { //uses key:value pair to check if the 'name' you are passing already exist in the 'names={}' object
+      names[name]++; //if it exist, increase(++) 'name' by 1
+    }
+    else {
+      names[name] = 1; //if it does not exist, assign '1' to the new 'name'
+    }
+
+  console.log(names); //displays the key:value pair map in the terminal
+  res.redirect('/names');
+});
+
+module.exports = router;
